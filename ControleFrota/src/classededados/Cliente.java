616f7d7e281353b9a055b23f9f11686a2e6b0c04
Pedro <Pedro@Pedro>
2017-06-22 11:05:55 -0300

    Cadastros funcionando

--- ControleFrota/src/classededados/Cliente.java
+++ ControleFrota/src/classededados/Cliente.java
@@ -5,8 +5,12 @@ public class Cliente {
     private String cpf = "";
     private String cnh = "";
     private String email = "";
-    private String telefone = "";
-
+    private String tipoTel1 = "";
+    private String tipoTel2 = "";
+    private String telefone1 = "";
+    private String telefone2 = "";
+    
+    
     public String getNome() {
         return nome;
     }
@@ -39,12 +43,36 @@ public class Cliente {
         this.email = email;
     }
 
-    public String getTelefone() {
-        return telefone;
+    public String getTipoTel1() {
+        return tipoTel1;
     }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
+    public void setTipoTel1(String tipoTel1) {
+        this.tipoTel1 = tipoTel1;
+    }
+
+    public String getTipoTel2() {
+        return tipoTel2;
+    }
+
+    public void setTipoTel2(String tipoTel2) {
+        this.tipoTel2 = tipoTel2;
+    }
+
+    public String getTelefone1() {
+        return telefone1;
+    }
+
+    public void setTelefone1(String telefone1) {
+        this.telefone1 = telefone1;
+    }
+
+    public String getTelefone2() {
+        return telefone2;
+    }
+
+    public void setTelefone2(String telefone2) {
+        this.telefone2 = telefone2;
     }
     
     
